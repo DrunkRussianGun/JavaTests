2aa0de988ff81dc47e4be013ce6b29ca09b6a25c
Drunk Russian Gun <drunkrussiangun@gmail.com>
2023-04-17 15:10:39 +0400

    Tested integer parsing

---
+++ src/test/unit/java/unittests/java/lang/IntegerTest.java
@@ -0,0 +1,27 @@
+package unittests.java.lang;
+
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.*;
+import org.junit.jupiter.params.provider.*;
+
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class IntegerTest {
+	@Test
+	public void parsesInMiddleOfString() {
+		String input = "t.fafer123:kkg";
+
+		int actual = Integer.parseInt(input, 7, 10, 10);
+
+		assertThat(actual, is(123));
+	}
+
+	@ParameterizedTest
+	@ValueSource(strings = { "t.2147483648:kkg", "t.4294967296:kkg" })
+	public void whenTooBigThrows(String input) {
+		//noinspection ResultOfMethodCallIgnored
+		assertThrows(NumberFormatException.class, () -> Integer.parseInt(input, 2, 12, 10));
+	}
+}
